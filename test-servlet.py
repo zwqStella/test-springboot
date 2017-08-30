d687fe8b2b93d9c31246449050fbbf33a148d0bd
钟文清 <zwqStella@hotmail.com>
2017-08-30 23:52:36 +0800

    add test

---
+++ test-servlet.py
@@ -0,0 +1,10 @@
+import urllib2
+
+r = urllib2.urlopen("http://127.0.0.1:8080/test/hello")
+
+result = r.read()
+
+if(result == "hello world"):
+	print("Test Succeeded!  " + result)
+else:
+	print("Test Failed" + result)
